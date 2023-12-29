d160f5abbcd6bb9b082c932002a455d56c64e387
Kerry Guarino <kerry.guarino@gmail.com>
2023-12-29 13:10:06 -0500

    getting an error with agencies

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,10 +5,30 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    # URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "https://data.cityofchicago.org/resource/ydr8-5enu.json"
+    # URL = "https://dev.socrata.com/foundry/data.cityofnewyork.us/szgz-awuh"
+
 
     response = requests.get(URL)
     return response.content
+  
+  def program_agencies(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs_list = []
+      programs = json.loads(self.get_programs())
+      for program in programs:
+        programs_list.append(program["agency"])
 
-programs = GetPrograms().get_programs()
-print(programs)
+      return programs_list
+
+# programs = GetPrograms().get_programs()
+# print(programs)
+  
+programs = GetPrograms()
+agencies = programs.program_agencies()
+
+for agency in set(agencies):
+  print(agency) 
+
+ 
